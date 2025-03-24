f20714b8841f1e32048623745477f8423df435c2
schkuromi <vgcrox@gmail.com>
2025-03-24 20:15:41 +0800

    updated to 3.11 (types, mod) logger changed to con

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,6 @@
 import { DependencyContainer } from "tsyringe";
 
 import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
-import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { ConfigServer } from "@spt/servers/ConfigServer";
 import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 import { ISeasonalEventConfig } from "@spt/models/spt/config/ISeasonalEventConfig";
@@ -10,9 +9,6 @@ class Mod implements IPostSptLoadMod
 {
     public postSptLoad(container: DependencyContainer): void
     {
-        // get logger
-        const logger = container.resolve<ILogger>("WinstonLogger");
-
         // get the config server so we can get a config with it
         const configServer = container.resolve<ConfigServer>("ConfigServer");
 
@@ -29,7 +25,7 @@ class Mod implements IPostSptLoadMod
         }
 
         // Log the new seasonal event setting
-        logger.log("[SCHKRM] Seasonal events are now off.", "yellow");
+        console.log("[SCHKRM] Seasonal events are now off.");
     }
 }
 
